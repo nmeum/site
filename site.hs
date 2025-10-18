aa73230386e7921704f769c36a2f83f4263cf4ef
Sören Tempel <soeren+git@soeren-tempel.net>
2025-10-18 04:23:24 +0200

    Cache sidebar and tags pages

--- site.hs
+++ site.hs
@@ -1,14 +1,21 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
-import           Data.Monoid (mappend)
+
 import           Hakyll
 import Data.Char (toUpper)
-import Text.Blaze ((!))
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
-import Data.Maybe (fromJust)
 import qualified Data.Map as Map
+import Hakyll.Core.Compiler.Internal
+  ( compilerUnsafeIO,
+    compilerTellCacheHits,
+    compilerTellDependencies,
+    compilerStore,
+    compilerAsk,
+    compilerUnderlying,
+    compilerProvider )
+import qualified Hakyll.Core.Store as Store
+import           Data.Typeable                 (Typeable)
+import           Data.Binary                   (Binary)
+import           System.Environment            (getProgName)
 
 captialize :: String -> String
 captialize "" = ""
@@ -30,7 +37,7 @@ tagsCtx tmap = listFieldWith "notes" noteCtx getPosts
        <> urlField "url"
        <> pathField "path"
        <> titleField "title"
-       <> field "count" (\(itemBody -> (t,_)) -> pure $ show $ countTag t tmap)
+       <> field "count" (\(itemBody -> (t,_)) -> pure (show $ countTag t tmap))
        <> missingField
  where
   getPosts :: Item (String,[Identifier]) -> Compiler [Item String]
@@ -40,6 +47,51 @@ tagsCtx tmap = listFieldWith "notes" noteCtx getPosts
   countTag tag map = maybe 0 length $ Map.lookup tag map
 
 --------------------------------------------------------------------------------
+
+-- This is a hack around the existing 'cached' function [1]. While the cache is
+-- largely undocumented [2], the existing function only works for resources
+-- (see 'Hakyll.Core.Provider') that are file-backed. This is due to the fact
+-- that 'resourceModified' is only defined for such resources. The 'cached'
+-- function uses 'resourceModified' for cache invalidation, it invalidates the
+-- cache if the resource was modified).
+--
+-- In our case, we want to cache a resource that is not file-backed.
+-- Specifically, the sidebar resource. Instead of relying on 'resourceModified'
+-- for cache invalidation we use the provided key for this purpose. That is,
+-- the resource is rebuild if an entry with the given key doesn't exist in the
+-- cache. In the case of the sidebar, the key corresponds to the sidebar items
+-- (i.e. the tags).
+--
+-- Ideally, we would want to express this in the dependency tree. That is, we
+-- want to tell Hakyll “don't rebuild the sidebar unless the tags changed”.
+-- However, we cann't do that because Hakyll—like make(1)—can only express
+-- dependencies on file content not metadata content [2]. To workaround that
+-- limitation, we use the cache.
+--
+-- [1]: https://github.com/jaspervdj/hakyll/blob/v4.16.7.1/lib/Hakyll/Core/Compiler.hs#L149-185
+-- [2]: https://github.com/jaspervdj/hakyll/issues/467
+-- [3]: https://github.com/jaspervdj/hakyll/issues/383#issuecomment-150836917
+cacheIfExists :: (Show k, Binary a, Typeable a) => k -> Compiler a -> Compiler a
+cacheIfExists key compiler = do
+    id'      <- compilerUnderlying <$> compilerAsk
+    store    <- compilerStore      <$> compilerAsk
+    provider <- compilerProvider   <$> compilerAsk
+
+    let k = [show key, show id']
+        go = compiler >>= \v -> v <$ compilerUnsafeIO (Store.set store k v)
+    compilerUnsafeIO (Store.get store k) >>= \r -> case r of
+        -- found: report cache hit and return value
+        Store.Found v   -> v <$ compilerTellCacheHits 1
+        -- not found: unexpected, but recoverable
+        Store.NotFound  -> go
+        -- other results: unrecoverable error
+        _               -> fail . error' =<< compilerUnsafeIO getProgName
+  where
+    error' progName =
+        "Hakyll.Core.Compiler.cached: Cache corrupt! " ++
+         "Try running: " ++ progName ++ " clean"
+
+------------------------------------------------------------------------
 
 main :: IO ()
 main = hakyll $ do
@@ -51,37 +103,53 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
-    -- One major limitations wrt. heavy relies on tags is that Hakyll doesn't
-    -- support dependencies on metadata of a file, rules always depend on the
-    -- entire file content. Therefore, if a post is changed, we need to rebuild
-    -- the sidebar and all tag patches (so basically the whole site).
-    --
-    -- See: https://github.com/jaspervdj/hakyll/issues/383#issuecomment-150836917
+    -- Hakyll's dependency tracking does not work well with resources depending
+    -- on metadata (such as tags, categories or dates). To reduces unnecessary
+    -- rebuilds because of post content (not metadata) changes, we build the
+    -- sidebar separately with custom caching (see 'cacheIfExists') above.
     tags <- buildTags "notes/*" (fromCapture "tags/*.html")
-    let tagAllCtx = listField "allTags" (tagsCtx $ Map.fromList (tagsMap tags)) (getAllTags tags)
+    create ["sidebar"] $ do
+      let tagAllCtx =
+            listField
+              "allTags"
+              (tagsCtx $ Map.fromList (tagsMap tags))
+              (getAllTags tags)
+
+      deps <- makePatternDependency (fromGlob "notes/*")
+      compile $ do
+        compilerTellDependencies [deps]
+        cacheIfExists (tagsMap tags) $ makeItem ""
+          >>= loadAndApplyTemplate "templates/sidebar.html" tagAllCtx
 
     match "notes/*" $ do
       route $ setExtension "html"
       compile $ do
+        sidebar <- constField "sidebar" <$> loadBody "sidebar"
+        let postTags = tagsField "tags" tags
+
         pandocCompiler
-          >>= loadAndApplyTemplate "templates/note.html" ((tagsField "tags" tags) <> noteCtx)
+          >>= loadAndApplyTemplate "templates/note.html" (postTags <> noteCtx)
           >>= saveSnapshot "content"
-          >>= loadAndApplyTemplate "templates/default.html" (tagAllCtx <> noteCtx)
+          >>= loadAndApplyTemplate "templates/default.html" (sidebar <> noteCtx)
           >>= relativizeUrls
 
     tagsRules tags $ \tagStr tagsPattern -> do
       route idRoute
       compile $ do
+        sidebar <- constField "sidebar" <$> loadBody "sidebar"
+        let pageTitle = constField "title" ("Tag: " ++ capitalize tagStr)
+
         notes <- loadAll tagsPattern >>= recentFirst
-        let notesCtx =
-              constField "title" ("Tag: " ++ capitalize tagStr)
+        let baseCtx = pageTitle <> sidebar <> defaultContext
+            listCtx = pageTitle
                 <> constField "tag" (capitalize tagStr)
                 <> listField "notes" noteCtx (return notes)
                 <> defaultContext
 
-        makeItem ""
-          >>= loadAndApplyTemplate "templates/notes.html" notesCtx
-          >>= loadAndApplyTemplate "templates/default.html" (tagAllCtx <> notesCtx)
+        notesMeta <- mapM getMetadata (map itemIdentifier notes)
+        cacheIfExists notesMeta $ makeItem ""
+          >>= loadAndApplyTemplate "templates/notes.html" listCtx
+          >>= loadAndApplyTemplate "templates/default.html" baseCtx
           >>= relativizeUrls
 
     match "templates/*" $ compile templateBodyCompiler
